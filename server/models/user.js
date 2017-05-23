a6b560da17f547eec07709ff239b2868b21dfd1f
Thenis <d.stoilov96@gmail.com>
2017-05-23 21:32:38 +0300

    Added user registration route. Added hashing password to user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 
 // User schema
@@ -14,7 +15,7 @@ let UserSchema = new mongoose.Schema({
 		type: String,
 		minlength: 6
 	},
-	tokens[{
+	tokens: [{
 		access: {
 			type: String,
 			required: true
@@ -27,4 +28,31 @@ let UserSchema = new mongoose.Schema({
 	}]
 });
 
+//middleware to run before the save function of an user
+UserSchema.pre("save", function(next) {
+	let user = this;
+	if (user.isModified("password")) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			})
+		})
+	} else{
+		next();
+	}
+
+});
+
+UserSchema.post("save", function (user) {
+	console.log(`User ${user.email} has been created`);
+})
+
+// Creating user model by giving the schema and the name of the model
 let User = mongoose.model("User", UserSchema);
+
+
+
+module.exports = {
+	User
+}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,38 @@
 const express = require("express");
+const _ = require("lodash");
+const bodyParser = require("body-parser");
 
 let mongoose = require("./db/mongoose.js");
+let {
+	User
+} = require("./models/user.js");
 
 let app = express();
 
 const port = 3000;
 
+//Parse incoming requsts to json middleware. Populates the req.body with params
+app.use(bodyParser.json());
+
+
+//Registration
+app.post("/users", (req, res) => {
+	let body = _.pick(req.body, ["email", "password"]); // get only the email and password from the request body
+
+	// create new user
+	let user = new User({
+		email: body.email,
+		password: body.password
+	});
+
+	user.save().then((user) => {
+		res.send(user);
+
+	}).catch((err) => res.status(400).send())
+});
+
 
 
 app.listen(port, () => {
 	console.log(`App starting on localhost:${port}`);
-})
+});
